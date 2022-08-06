b238150f058c954d4276f3a8eef836e96d8458bd
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-06 22:10:05 +0300

    Adding MySQL support.

--- src/main/java/merkanto/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/merkanto/sdjpaintro/bootstrap/DataInitializer.java
@@ -1,24 +1,28 @@
 package merkanto.sdjpaintro.bootstrap;
 
-import lombok.RequiredArgsConstructor;
 import merkanto.sdjpaintro.domain.Book;
 import merkanto.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
+
+    public DataInitializer(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
 
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+
         System.out.println("Id: " + bookDDD.getId() );
 
         Book savedDDD = bookRepository.save(bookDDD);
+
         System.out.println("Id: " + savedDDD.getId() );
 
         Book bookSIA = new Book("Spring In Action", "234234", "Oriely");

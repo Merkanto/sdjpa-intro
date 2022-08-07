5b04e5f012df131866a34fdf208d42649c08ddc0
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-07 19:29:35 +0300

    Adding Natural PK.

---
+++ src/main/java/merkanto/sdjpaintro/repositories/BookNaturalRepository.java
@@ -0,0 +1,7 @@
+package merkanto.sdjpaintro.repositories;
+
+import merkanto.sdjpaintro.domain.BookNatural;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookNaturalRepository extends JpaRepository<BookNatural, String> {
+}
